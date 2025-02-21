6c9ca6f19e633e489178ff7061db24d2ea99697f
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2025-02-22 01:11:17 +0300

    .

---
+++ UnitOfWorkDesignPattern/DesignPattern.UnitOfWork/Controllers/DefaultController.cs
@@ -0,0 +1,45 @@
+﻿using DesignPattern.BusinessLayer.Abstract;
+using DesignPattern.EntityLayer.Concrete;
+using DesignPattern.UnitOfWork.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DesignPattern.UnitOfWork.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        private readonly ICustomerService _customerService;
+
+        public DefaultController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CustomerViewModel model)
+        {
+            var values1 = _customerService.TGetById(model.SenderId);
+            var values2 = _customerService.TGetById(model.ReceiverId);
+
+            values1.CustomerBalance -= model.Amount;
+            values2.CustomerBalance += model.Amount;
+
+            List<Customer> customers = new List<Customer>()
+            {
+                values1,
+                values2
+            };
+
+            _customerService.TMultiUpdate(customers);
+
+            return View();
+        }
+    }
+}
